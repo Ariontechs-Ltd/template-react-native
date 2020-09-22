3551460e5d020dddd3ba510b4d3ef42f0c60138e
ariontechs <nat@ariontechs.com>
2020-09-23 00:12:09 +0800

    Modify the useTempate

--- src/hooks/useTemplate.js
+++ src/hooks/useTemplate.js
@@ -4,7 +4,7 @@ import { useNavigation , useRoute} from '@react-navigation/native'
 
 export default () => {
 
-    const [tState, tActions] = React.useContext(TemplateContext)
+    const [state, actions] = React.useContext(TemplateContext)
     const navigation = useNavigation()
     const route = useRoute()
 
